df4fa82710bcc6d2c03e2c71c60fa9421444911d
Urizen <taraszotkin93@gmail.com>
2018-09-26 20:53:36 +0300

    done

--- SkillsawDateTimeTask/Program.cs
+++ SkillsawDateTimeTask/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SkillsawDateTimeTask
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            const string mysqlDateFormat = "yyyy-MM-dd";
+
+            //task does not specify should time be local or UTC so i assume a local is fine
+            var now = DateTimeOffset.Now;
+            var nextMonthFifteenthDay = now.AddMonths(1).AddDays(15 - now.Day);
+            var prevMonthFifteenthDay = now.AddMonths(-1).AddDays(15 - now.Day);
+
+            Console.WriteLine(nextMonthFifteenthDay.ToString(mysqlDateFormat));
+            Console.WriteLine(prevMonthFifteenthDay.ToString(mysqlDateFormat));
+
+            Console.ReadLine();
         }
     }
 }
